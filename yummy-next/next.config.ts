24294d04b49ae7e74be9e66a3bb43a9e5f93bd08
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2025-04-11 12:25:24 +0900

    initiate commit
    initiate commit

---
+++ yummy-next/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default nextConfig;
