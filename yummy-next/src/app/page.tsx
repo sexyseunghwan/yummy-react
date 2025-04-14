67a05353bbe3cdde91357144d17799cea4e087ce
sexyseunghwan <ssh9308@naver.com>
2025-04-14 23:03:20 +0900

    테스트
    테스트

--- yummy-next/src/app/page.tsx
+++ yummy-next/src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Script from 'next/script';
+import axios from 'axios';
 
 /* Declare naver as a global variable */
 // declare const naver: any;
@@ -12,47 +13,49 @@ import { Store } from '@/types/store';
 
 
 export default function YummyMap() {
-	/* api 경로 */
 	const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL;
 	const [stores, setStores] = useState<Store[]>([]);
 	const [mapInstance, setMapInstance] = useState<any>(null);
 	const [markers, setMarkers] = useState<any[]>([]);
 	const [zeroPayMarkers, setZeroPayMarkers] = useState<any[]>([]);
-	
+
 	useEffect(() => {
 		async function fetchStores() {
 			const lngx = 127.0465;
 			const laty = 37.5032;
 			const defaultStore = { name: '알바천국', lat: laty, lng: lngx, type: 'company' };
 
-			const res = await fetch(`${apiBaseUrl}/search/allData`);
-			
-			if (!res.ok) {
-				console.error("API 응답 에러:", res.status);
-				return;
+			try {
+				const res = await axios.get(`${apiBaseUrl}/search/allData`);
+				const data = res.data;
+
+				const converted = data.map((s: any) => ({
+					name: s.name,
+					lat: s.lat,
+					lng: s.lng,
+					type: s.type,
+					isBeefulPay: s.zero_possible,
+				}));
+
+				const allStores = [defaultStore, ...converted];
+				setStores(allStores);
+
+				const { map, markers, zeroPayMarkers } = initMap(allStores);
+				setMapInstance(map);
+				setMarkers(markers);
+				setZeroPayMarkers(zeroPayMarkers);
+			} catch (error) {
+				if (axios.isAxiosError(error)) {
+					console.error('Axios API 요청 에러:', error.response?.status, error.message);
+				} else {
+					console.error('예상치 못한 에러:', error);
+				}
 			}
-			const data = await res.json();
-
-			const converted = data.map((s: any) => ({
-				name: s.name,
-				lat: s.lat,
-				lng: s.lng,
-				type: s.type,
-				isBeefulPay: s.zero_possible,
-			}));
-
-			const allStores = [defaultStore, ...converted];
-			setStores(allStores);
-			
-			const { map, markers, zeroPayMarkers } = initMap(allStores); 
-			setMapInstance(map);
-			setMarkers(markers);
-			setZeroPayMarkers(zeroPayMarkers);
 		}
 
 		fetchStores();
 	}, []);
-
+	
   return (
     <>
 		<Script
