5abbd578b68af6d1c80161b85d9017856413ff37
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2025-04-16 12:09:42 +0900

    리다이렉트 오류 해결 & 이미지 태그 변경
    리다이렉트 오류 해결 & 이미지 태그 변경

--- yummy-next/src/app/login/LoginForm.tsx
+++ yummy-next/src/app/login/LoginForm.tsx
@@ -44,19 +44,19 @@ export default function Login() {
             <button id="login-button" onClick={handleLogin}>로그인</button>
 
             <a className="oauth-login" id="oauth-kakao" onClick={loginWithKakao}>
-                <Image src="https://upload.wikimedia.org/wikipedia/commons/e/e3/KakaoTalk_logo.svg" alt="KakaoTalk Logo" />
+                <Image src="/images/oauth/kakao.svg" alt="KakaoTalk Logo" width={24} height={24}/>
                 카카오톡으로 로그인
             </a>
             <a className="oauth-login" id="oauth-naver" onClick={loginWithNaver}>
-                <Image src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAASFBMVEVHcEwD0mcE62cE6GcE7mcE7GcF7GcE4mdd4pE82H4C2mX////g9+rR9OCd57sC0mYCymVd0Y8Cw2UixnACvWYCuWYCt2YCuGa1Zq98AAAAGHRSTlMAS7X//73A//////////////////+1/78LKXJJAAAA0UlEQVR4AXWJhRHAQBACibtb/52GP+KyLzAsiOcH4YPA92BETyNikDj4IgyCCPDU9O9FePCDncTuBR/JlUCxI/nkKdM0zSzJIfPETlqQjKVkptuIXJgsWSqT4iJJ/ZBNbkeyyBpJrWj4k02Wkg3hDIakqCUFFK2TVV2Ibpdt407TOdmWkqnGFq2QzCQ7bQ/Z1/+yb/vyJnvhZMkcTPZil0PXddnIkrEMD9mPREF2ySr6oxwV0/jLBG9Wm3n0H9UDltkYFZe+ALIP5AxveqvJo1gBuZAlPbYzv2wAAAAASUVORK5CYII=" alt="KakaoTalk Logo"/>
+                <Image src="/images/oauth/naver.png" alt="KakaoTalk Logo" width={24} height={24}/>
                 네이버로 로그인
             </a>
             <a className="oauth-login" id="oauth-telegram" onClick={loginWithTelegram}>
-                <Image src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAY1BMVEX////8/f+41Pd7sfFFmO0gius9le1zrfCuzvby9/2QvPMTh+srjewzkOwykOwMhuvV5frB2figxvW20vfq8v1np/A4kuzv9f3i7fxtqvDO4fmWwPRQne7L3/kAguqGt/Ld6/s4puGjAAAA/0lEQVR4AayRhQ5DMQhFeVKFls5d/v8rt9I1YRLfjcLBgT9oGKfZGOt8+EYRDaWcExmePnAhk1MX4ajZgpNW5qXKY0lT4lVnA1X7I3cNTdEkJYNY+25eiahn2S53u0p5L3DsiZn4cAQAgXQSOFFjxpx2AAGgmBp5FjhnqWd9beGOvRQKrOvjptaDCxcAOJHA8IJmbA2uMoVT0D4b5Ntlt8i0g+YQCD0wk0GDd7EHFmjF8H0V2kilO4oRBQZOndbefdgCatGqjIcBbtWkMzQFfT9qs3L59c4sDNVDlx8PxQmURlY/Ja55SusNU24Iz3f41P4041M2FvitEB4jPp0DAHyFFi4xND68AAAAAElFTkSuQmCC" alt="KakaoTalk Logo"/>
+                <Image src="/images/oauth/telegram.png" alt="KakaoTalk Logo" width={24} height={24}/>
                 텔레그램으로 로그인
             </a>
             <a className="oauth-login" id="oauth-google" onClick={loginWithGoogle}>
-                <Image src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google Logo" style={{ width: '70px' }} />
+                <Image src="/images/oauth/google.png" alt="Google Logo" width={24} height={24} style={{ width: '70px' }} />
                 구글로 로그인
             </a>
         </>
--- yummy-next/src/components/Header.tsx
+++ yummy-next/src/components/Header.tsx
@@ -3,35 +3,38 @@
 import Link from 'next/link';
 
 export default function Header() {
-  const toggleSidebar = () => {
-    const sidebar = document.getElementById('sidebar');
-    sidebar?.classList.toggle('active');
-  };
+	
+	const toggleSidebar = () => {
+		const sidebar = document.getElementById('sidebar');
+		sidebar?.classList.toggle('active');
+	};
 
-  return (
-    <header>
-      <h1>
-		<Link href="/" passHref>
-			<span style={{ textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}>
-			가야 할 지도
-			</span>
-		</Link>
-      </h1>
+	const redirectToHome = () => {
+		window.location.href = '/';
+	}
 
-      <div className="header-search-container">
-        <input
-          type="text"
-          id="headerSearchInput"
-          placeholder="검색할 가게명을 입력하세요!"
-        />
-        <button className="header-search-button" onClick={() => {}}>
-          검색
-        </button>
-      </div>
+	return (
+		<header>
+			<h1>
+				<a style={{ textDecoration: 'none', color: 'inherit', cursor: 'pointer' }} onClick={redirectToHome}>
+					가야 할 지도
+        		</a>
+			</h1>
 
-      <div className="hamburger-menu" onClick={toggleSidebar}>
-        ☰
-      </div>
-    </header>
-  );
+			<div className="header-search-container">
+			<input
+				type="text"
+				id="headerSearchInput"
+				placeholder="검색할 가게명을 입력하세요!"
+			/>
+			<button className="header-search-button" onClick={() => {}}>
+				검색
+			</button>
+			</div>
+
+			<div className="hamburger-menu" onClick={toggleSidebar}>
+				☰
+			</div>
+		</header>
+	);
 }
