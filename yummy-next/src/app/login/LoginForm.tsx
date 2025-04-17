f5fb1471f26ea796ca82772f9042317ded65c648
seung hwan shin <67136763+sexyseunghwan@users.noreply.github.com>
2025-04-17 11:42:28 +0900

    운영서버 에러확인용
    운영서버 에러확인용

--- yummy-next/src/app/login/LoginForm.tsx
+++ yummy-next/src/app/login/LoginForm.tsx
@@ -25,7 +25,8 @@ export default function Login() {
                                     withCredentials: true,
                                 }
                             );
-            
+            console.log("response: " + response);
+
             if (response.status === 200) {
                 window.location.href = '/';
             } else {
