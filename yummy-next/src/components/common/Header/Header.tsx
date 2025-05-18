cf72f66628f7ec6a20bfc1b436bfd75c5bdd88f0
local-Leee <locallee.c@gmail.com>
2025-05-18 20:34:07 +0900

    Header에 Logo 아이콘 추가

--- yummy-next/src/components/common/Header/Header.tsx
+++ yummy-next/src/components/common/Header/Header.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { Button } from '@/components/common/Button/Button';
 import Input from '@/components/common/Input/Input';
 import MenuIcon from '@/components/common/Icons/MenuIcon';
+import LogoIcon from '@/components/common/Icons/LogoIcon';
 import type { HeaderProps } from './Header.types';
 
 const Header = ({ 
@@ -12,7 +13,7 @@ const Header = ({
 
 	return (
 		<header className="flex items-center justify-between gap-3 border-b border-accent py-2 px-2 fixed top-0 left-0 w-full z-[999] bg-background">
-			<h1><Link href="/">로고</Link></h1>
+			<h1><Link href="/"><LogoIcon /></Link></h1>
 
 			<div className="flex flex-1 items-center gap-2">
 				<Input placeholder="검색할 가게명을 입력하세요."/>
