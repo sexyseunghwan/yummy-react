2553503bfb7e6b125a3c6eb42aee765749f163c1
jisunyoon <83256748+jisunyoon@users.noreply.github.com>
2025-06-22 18:11:53 +0900

    feat(front): search 페이지 추가중

--- yummy-next/src/components/common/Header/Header.tsx
+++ yummy-next/src/components/common/Header/Header.tsx
@@ -1,32 +1,26 @@
 'use client';
 
 import Link from 'next/link';
-import { ComboBox } from '../ComboBox';
+import Input from '../Input/Input';
 import MenuIcon from '@/components/common/Icons/MenuIcon';
 import LogoIcon from '@/components/common/Icons/LogoIcon';
 import type { HeaderProps } from './Header.types';
-import { useState } from 'react';
 
 const Header = ({ 
 	onMenuClick
  }: HeaderProps) => {
-	const [selected, setSelected] = useState<string>("");
 	return (
 		<header className="flex items-center justify-between gap-3 border-b border-accent py-2 px-2 w-full bg-background">
 			<h1><Link href="/"><LogoIcon /></Link></h1>
 
 			<div className="flex flex-1 items-center gap-2">
-				<ComboBox 
-					selected={selected}
-					onChange={(value) => setSelected(value)}
-				>
-					<ComboBox.Trigger />
-					<ComboBox.Content>
-						{/* <ComboBox.Item value="맛집 추천">맛집 추천</ComboBox.Item>
-						<ComboBox.Item value="카페 추천">카페 추천</ComboBox.Item>
-						<ComboBox.Item value="디저트 추천">디저트 추천</ComboBox.Item> */}
-					</ComboBox.Content>
-				</ComboBox>
+				<Link href="/search">
+					<Input 
+						placeholder="검색어를 입력해주세요."
+						size="small"
+						readOnly
+					/>
+				</Link>
 			</div>
 
 			<button
--- yummy-next/src/components/common/Input/Input.tsx
+++ yummy-next/src/components/common/Input/Input.tsx
@@ -12,7 +12,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(({
 	...props
 }, ref) => {
 
-    const inputStyle = 'w-full outline-none transition-colors border border-accent bg-background text-text focus:border-primary disabled:cursor-not-allowed disabled:opacity-50';
+    const inputStyle = 'flex items-center w-full h-10 rounded-md border border-accent/40 bg-white text-slate-800 shadow-sm hover:bg-slate-50 transition-all duration-200 outline-none';
 
 	const inputSizes = {
 		small: 'px-2 py-2 text-sm rounded-md',
